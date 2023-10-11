d63fe0b19a400f5c36b4857e6a8fd050307db1f9
Kristophe Sankar <kristophe.lukas.sankar@gmail.com>
2023-10-11 19:42:02 -0400

    init project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,79 @@
-import "./styles.css";
+import React, {useState} from 'react';
+import moment from "moment";
+import Timeline from "react-calendar-timeline";
+import generateFakeData from "./lib/generate-fake-data";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+var keys = {
+  groupIdKey: "id",
+  groupTitleKey: "title",
+  groupRightTitleKey: "rightTitle",
+  itemIdKey: "id",
+  itemTitleKey: "title",
+  itemDivTitleKey: "title",
+  itemGroupKey: "group",
+  itemTimeStartKey: "start",
+  itemTimeEndKey: "end",
+  groupLabelKey: "title"
+};
+
+export default function App () {
+
+  const data = generateFakeData();
+  const start = moment().startOf("day").toDate();
+  const end = moment().startOf("day").add(1, "day") .toDate();
+
+  const [groups, setGroups] = useState(data.groups);
+  const [items, setItems] = useState(data.items);
+  const [defaultTimeStart, setDefaultTimeStart] = useState(start);
+  const [defaultTimeEnd, setDefaultTimeEnd] = useState(end);
+
+
+  const handleItemMove = (itemId, dragTime, newGroupOrder) => {
+    const group = groups[newGroupOrder];
+    let newItems = items.map(item =>
+        item.id === itemId
+          ? Object.assign({}, item, {
+              start: dragTime,
+              end: dragTime + (item.end - item.start),
+              group: group.id
+            })
+          : item
+      )
+
+    setItems(newItems)
+    console.log("Moved", itemId, dragTime, newGroupOrder);
+  };
+
+  const handleItemResize = (itemId, time, edge) => {
+
+     let newItems = items.map(item =>
+        item.id === itemId
+          ? Object.assign({}, item, {
+              start: edge === "left" ? time : item.start,
+              end: edge === "left" ? item.end : time
+            })
+          : item
+      )
+
+    setItems(newItems)
+    console.log("Resized", itemId, time, edge);
+  };
+
+    return (
+      <Timeline
+        groups={groups}
+        items={items}
+        keys={keys}
+        fullUpdate
+        itemTouchSendsClick={false}
+        stackItems
+        itemHeightRatio={0.75}
+        canMove={true}
+        canResize={"both"}
+        defaultTimeStart={defaultTimeStart}
+        defaultTimeEnd={defaultTimeEnd}
+        onItemMove={handleItemMove}
+        onItemResize={handleItemResize}
+      />
+    );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import 'react-calendar-timeline/lib/Timeline.css'
 
 import App from "./App";
 
